7844a9f6cd0d309759cbce324ecbfc3f76b7f01a
dami0alt <daltamiranoontiveros@gmail.com>
2025-11-10 11:44:33 +0100

    feature: Dialog message implementation
    I implemented four buttons that return the message inserted in the text field as a different types of dialogs messages.

---
+++ IA03/Actividad_01/lib/main.dart
@@ -0,0 +1,143 @@
+import 'dart:ui_web';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Recuperar el valor d' 'un camp de text',
+      home: MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Recuperar el valor d\'un camp de text'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            controller: myController,
+          ),
+        ),
+        floatingActionButton:
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                barrierDismissible: true,
+                builder: (context) {
+                  return SimpleDialog(
+                      title: const Text("SimpleDialog"),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      children: [
+                        Padding(
+                          padding:
+                              EdgeInsetsGeometry.symmetric(horizontal: 24.0),
+                          child: Text(myController.text),
+                        ),
+                      ]);
+                },
+              );
+            },
+            tooltip: 'Show Simple Dialog',
+            child: const Icon(Icons.text_fields),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: const Text("AlertDialog"),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0)),
+                      content: Text(myController.text),
+                      actions: [
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text("OK"),
+                        )
+                      ],
+                    );
+                  });
+            },
+            tooltip: 'Show Alert Dialog',
+            child: const Icon(Icons.text_fields),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text(myController.text),
+                duration: Duration(seconds: 2),
+              ));
+            },
+            tooltip: 'Show Snack Bar',
+            child: const Icon(Icons.text_fields),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                  backgroundColor: Colors.transparent,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Container(
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(25),
+                            topRight: Radius.circular(25),
+                          ),
+                        ),
+                        height: 200,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Text(myController.text),
+                              ElevatedButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: const Text('Tancar BottomSheet'),
+                              )
+                            ],
+                          ),
+                        ));
+                  });
+            },
+            tooltip: 'Mostra el valor!',
+            child: const Icon(Icons.text_fields),
+          ),
+        ]));
+  }
+}
