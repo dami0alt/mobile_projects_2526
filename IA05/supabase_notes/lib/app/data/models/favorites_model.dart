9bb716750d66d8d8cd33ab85d4b4676f423ab148
dami0alt <daltamiranoontiveros@gmail.com>
2026-02-08 13:15:30 +0100

    feat: model classes: Album, Listener and Favorite created
    I created the main model classes to serialize and deserialize the json responses from the Supabase API request.

---
+++ IA05/supabase_notes/lib/app/data/models/favorites_model.dart
@@ -0,0 +1,33 @@
+import 'package:supabase_notes/app/data/models/albums_model.dart';
+
+class Favorite {
+  String idAlbum;
+  String idListener;
+  String? savedAt;
+  Album? album;
+
+  Favorite(
+      {required this.idAlbum,
+      required this.idListener,
+      required this.savedAt,
+      this.album});
+
+  Favorite.fromJson(Map<String, dynamic> json)
+      : idAlbum = json['album_id'] ?? "",
+        idListener = json['listener_id'] ?? "Unknown",
+        savedAt = json['saved_at'] ?? "Unknown",
+        album = json['albums'] != null ? Album.fromJson(json['albums']) : null;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'album_id': idAlbum,
+      'listener_id': idListener,
+      'saved_at': savedAt,
+    };
+  }
+
+  static List<Favorite> fromJsonList(List? data) {
+    if (data == null || data.isEmpty) return [];
+    return data.map((e) => Favorite.fromJson(e)).toList();
+  }
+}
---
+++ IA05/supabase_notes/lib/app/data/models/listeners_model.dart
@@ -0,0 +1,40 @@
+class Listener {
+  String id;
+  String firstName;
+  String lastName;
+  String email;
+  String? photoUrl;
+  String createdAt;
+
+  Listener(
+      {required this.id,
+      required this.firstName,
+      required this.lastName,
+      required this.email,
+      this.photoUrl,
+      required this.createdAt});
+
+  Listener.fromJson(Map<String, dynamic> json)
+      : id = json['id'] ?? "",
+        firstName = json['fname'] ?? "Unknown",
+        lastName = json['lname'] ?? "Unknown",
+        email = json['email'] ?? "",
+        photoUrl = json['photo_url'] ?? "",
+        createdAt = json['created_at'] ?? "Unknown";
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'fname': firstName,
+      'lname': lastName,
+      'email': email,
+      'photo_url': photoUrl,
+      'created_at': createdAt,
+    };
+  }
+
+  static List<Listener> fromJsonList(List? data) {
+    if (data == null || data.isEmpty) return [];
+    return data.map((e) => Listener.fromJson(e)).toList();
+  }
+}
